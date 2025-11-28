110b1a30abc99347179ffc2627b2b71ec862ef8f
SaikouTrawally <B00161930@mytudublin.ie>
2025-11-28 12:00:00 +0000

    Added Pull-to-Refresh on Fixtures Page

--- FixturesScreen.js
+++ FixturesScreen.js
@@ -1,15 +1,24 @@
-// FixturesScreen.js
-import React from 'react';
-import { View, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList, RefreshControl } from 'react-native';
 import { matches } from './mock';
 import MatchCard from './components/MatchCard';
 
 export default function FixturesScreen({ navigation }) {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => setRefreshing(false), 1200);
+  };
+
   return (
     <View style={{ flex: 1, padding: 16 }}>
       <FlatList
         data={matches}
         keyExtractor={i => i.id}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
         renderItem={({ item }) => (
           <MatchCard
             item={item}
@@ -22,4 +31,5 @@ export default function FixturesScreen({ navigation }) {
     </View>
   );
 }
+
 
