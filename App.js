149bebf0944565ef22376bff0d9e12d3c0b3b110
SaikouTrawally <B00161930@mytudublin.ie>
2025-11-28 11:25:03 +0000

    Added Bottom Tab Navigation Home, Fixtures, Favourites, Settings

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
+// App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './HomeScreen';
 import FixturesScreen from './FixturesScreen';
 import MatchDetailsScreen from './MatchDetailsScreen';
 import FavouritesScreen from './FavouritesScreen';
 import SettingsScreen from './SettingsScreen';
-import WhatScreen from './WhatScreen'; 
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Fixtures" component={FixturesScreen} />
+      <Tab.Screen name="Favourites" component={FavouritesScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Fixtures" component={FixturesScreen} />
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={Tabs} options={{ headerShown: false }} />
         <Stack.Screen name="MatchDetails" component={MatchDetailsScreen} />
-        <Stack.Screen name="Favourites" component={FavouritesScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-        <Stack.Screen name="What" component={WhatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
